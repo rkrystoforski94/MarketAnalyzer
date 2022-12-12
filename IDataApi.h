253f673b4c04b19209f553cc5bffb6affa1e674b
rkrystoforski94 <rkrystoforski94@gmail.com>
2022-12-12 22:01:52 +0100

    Add general interface for playing with our Data

---
+++ IDataApi.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <sstream>
+
+
+namespace data_api
+{
+// General interface for play with our data
+class IDataApi
+{
+public:
+	virtual int getData() = 0;
+
+	virtual std::stringstream printData() = 0;
+
+	virtual int convertFromJson(void* data) = 0;
+private:
+	void* data;
+};
+
+
+} // namespace data_api
