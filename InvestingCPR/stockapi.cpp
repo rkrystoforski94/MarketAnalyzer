af40b5eee283b06b2071b755a4db0a3360af79a3
ziajek444 <ziajkowski.marcin@gmail.com>
2023-02-04 21:10:32 +0100

    Added comments TODO for new lib (FastHtml).

--- InvestingCPR/stockapi.cpp
+++ InvestingCPR/stockapi.cpp
@@ -10,10 +10,26 @@
 #include <string>
 #include <cpr/cpr.h>
 
+// TODO : FastHTML
+// multi filter is required
+// GetListedData(); // return_not_empty() method would be useful
+// execute not from ctor would be more handy
+// "Only first found" option would speed up work in certain cases
+// queue is important so thread needs to work on separate containers
+// space sensetive would be helpful <tag ... attr1 = "n a me" attr2 = "nam e" ...>
+// "n a me" != "nam e"
+// GetAllTagOpenIndexes() should filter out wrong tags (not append if not match)
+// !! remove comments
+
 #include "../../FastHTML/include/FastHTML.h"
+
+#include <array>
+#include <algorithm>
+#include <cctype>
 
 void get_response(std::string url, std::shared_ptr<std::string> resp);
 void get_tagdata_from_response(std::vector<const char*> tagClass, std::shared_ptr<std::string> resp, std::string& data);
+std::string removeBrackets(std::string str);
 
 
 // DLL internal state variables:
@@ -62,7 +78,6 @@ bool sa::test_request()
 std::string sa::get_stock_price(std::string stockName)
 {
     std::string url = "https://www.investing.com/equities/" + stockName;
-    //url.append(stockName);
     std::shared_ptr<std::string> response = std::make_shared<std::string>();
     get_response(url, response);  // set response
 
@@ -71,73 +86,115 @@ std::string sa::get_stock_price(std::string stockName)
     filter.second["data-test"] = "instrument-price-last";
     HResponse obj1(response.get(), filter);
     _getStockPrice = obj1.GetLastData();
-    if (_getStockPrice == "UPS") { // To fast might cause missing data, not full html site
-        std::vector<const char*> divTag;
-        divTag.push_back("instrument-price-last");
-        get_tagdata_from_response(divTag, response, _getStockPrice);
-    }
-
-    //std::vector<const char*> divTag;
-    //divTag.push_back("instrument-price-last");
-    //get_tagdata_from_response(divTag, response, _getStockPrice);
 
     return _getStockPrice;
 }
 
 std::string sa::get_prev_close_price(std::string stockName)
 {
-    std::string url = "https://www.investing.com/equities/";
-    url.append(stockName);
+    std::string url = "https://www.investing.com/equities/" + stockName;
     std::shared_ptr<std::string> response = std::make_shared<std::string>();
     get_response(url, response);  // set response
 
-    std::vector<const char*> divTag;
-    divTag.push_back("data-test=\"prevClose\"");
-    divTag.push_back("class=\"key-info_dd-numeric__5IsvY\"");
-    divTag.push_back("span");
-    get_tagdata_from_response(divTag, response, _getPrevClosePrice);
+    //<dd data-test="sharesOutstanding" class="font-bold">
+    // <span class="key-info_dd-numeric__5IsvY"><span>
+    //
+    std::pair<std::string, std::map<std::string, std::string>> filter;
+    filter.first = "dd";
+    filter.second["data-test"] = "prevClose";
+    filter.second["class"] = "font-bold";
+    HResponse obj1(response.get(), filter);
+    std::string s = obj1.GetLastData();
 
+    filter.first = "span";
+    filter.second.clear();
+    HResponse obj2(&s, filter);
+    auto resps = obj2.GetListedData();
+    _getPrevClosePrice = "UPS2";
+    for (auto var : resps)
+    {
+        if (var != "") {
+            _getPrevClosePrice = var;
+            break;
+        }
+    }
     return _getPrevClosePrice;
 }
 
 std::string sa::get_open_price(std::string stockName)
 {
-    std::string url = "https://www.investing.com/equities/";
-    url.append(stockName);
+    std::string url = "https://www.investing.com/equities/" + stockName;
     std::shared_ptr<std::string> response = std::make_shared<std::string>();
     get_response(url, response);  // set response
 
-    std::vector<const char*> divTag;
-    divTag.push_back("data-test=\"open\"");
-    divTag.push_back("class=\"key-info_dd-numeric__5IsvY\"");
-    divTag.push_back("span");
-    get_tagdata_from_response(divTag, response, _getOpenPrice);
+    // <dd data-test="open" class="font-bold"><span class="key-info_dd-numeric__5IsvY">
+    // <span>183.95</span><span></span></span></dd>
+    //
+    std::pair<std::string, std::map<std::string, std::string>> filter;
+    filter.first = "dd";
+    filter.second["data-test"] = "open";
+    filter.second["class"] = "font-bold";
+    HResponse obj1(response.get(), filter);
+    std::string s = obj1.GetLastData();
+
+    filter.first = "span";
+    filter.second.clear();
+    HResponse obj2(&s, filter);
+    auto resps = obj2.GetListedData();
+    _getOpenPrice = "UPS2";
+    for (auto var : resps)
+    {
+        if (var != "") {
+            _getOpenPrice = var;
+            break;
+        }
+    }
 
     return _getOpenPrice;
 }
 
 std::string sa::get_day_range_price(std::string stockName)
 {
-    std::string url = "https://www.investing.com/equities/";
-    url.append(stockName);
+    std::string url = "https://www.investing.com/equities/" + stockName;
     std::shared_ptr<std::string> response = std::make_shared<std::string>();
     get_response(url, response);  // set response
 
     std::string min;
     std::string max;
 
-    std::vector<const char*> divTag;
-    divTag.push_back("data-test=\"dailyRange\"");
-    divTag.push_back("class=\"key-info_dd-numeric__5IsvY\"");
-    divTag.push_back("span");
-    get_tagdata_from_response(divTag, response, min);
+    //<dd data-test="dailyRange" class="font-bold">
+    // <span class="key-info_dd-numeric__5IsvY">
+    // <span>359</span>
+    // <span></span></span>
+    // <span class="ml-1 mr-1">-</span>
+    // <span class="key-info_dd-numeric__5IsvY">
+    // <span>379.43</span>
+    // <span></span></span>
+    //</dd>
+    //
+    std::pair<std::string, std::map<std::string, std::string>> filter;
+    filter.first = "dd";
+    filter.second["data-test"] = "dailyRange";
+    filter.second["class"] = "font-bold";
+    HResponse obj1(response.get(), filter);
+    std::string s = obj1.GetLastData();
 
-    divTag.clear();
-    divTag.push_back("data-test=\"dailyRange\"");
-    divTag.push_back("class=\"key-info_dd-numeric__5IsvY\"");
-    divTag.push_back("class=\"key-info_dd-numeric__5IsvY\"");
-    divTag.push_back("span");
-    get_tagdata_from_response(divTag, response, max);
+    filter.first = "span";
+    filter.second.clear();
+    HResponse obj2(&s, filter);
+
+    auto allPrices = obj2.GetListedData();
+    std::array<std::string, 2> pricesArr;
+    unsigned int index = 0;
+    for (auto val : allPrices) {
+        if (val != "") {
+            pricesArr[index++] = val;
+        }
+        if (index >= 2) break;
+    }
+    std::sort(pricesArr.begin(), pricesArr.end());
+    min = pricesArr[0];
+    max = pricesArr[pricesArr.size() - 1];
 
     _getDayRangePrice = min;
     _getDayRangePrice.append(" - " + max);
@@ -147,26 +204,46 @@ std::string sa::get_day_range_price(std::string stockName)
 
 std::string sa::get_52w_range_price(std::string stockName)
 {
-    std::string url = "https://www.investing.com/equities/";
-    url.append(stockName);
+    std::string url = "https://www.investing.com/equities/" + stockName;
     std::shared_ptr<std::string> response = std::make_shared<std::string>();
     get_response(url, response);  // set response
 
     std::string min;
     std::string max;
 
-    std::vector<const char*> divTag;
-    divTag.push_back("data-test=\"weekRange\"");
-    divTag.push_back("class=\"key-info_dd-numeric__5IsvY\"");
-    divTag.push_back("span");
-    get_tagdata_from_response(divTag, response, min);
+    // 
+    //<dd data-test="weekRange" class="font-bold">
+    // <span class="key-info_dd-numeric__5IsvY">
+    // <span>162.71</span><span></span></span>
+    // <span class="ml-1 mr-1">-</span>
+    // <span class="key-info_dd-numeric__5IsvY">
+    // <span>412.98</span><span></span></span>
+    //</dd>
+    //
+    std::pair<std::string, std::map<std::string, std::string>> filter;
+    filter.first = "dd";
+    filter.second["data-test"] = "weekRange";
+    filter.second["class"] = "font-bold";
+    HResponse obj1(response.get(), filter);
+    std::string s = obj1.GetLastData();
 
-    divTag.clear();
-    divTag.push_back("data-test=\"weekRange\"");
-    divTag.push_back("class=\"key-info_dd-numeric__5IsvY\"");
-    divTag.push_back("class=\"key-info_dd-numeric__5IsvY\"");
-    divTag.push_back("span");
-    get_tagdata_from_response(divTag, response, max);
+    filter.first = "span";
+    filter.second.clear();
+    HResponse obj2(&s, filter);
+
+    auto allPrices = obj2.GetListedData();
+    std::array<std::string, 2> pricesArr;
+    unsigned int index = 0;
+    for (auto val : allPrices) {
+        if (val != "") {
+            pricesArr[index++] = val;
+        }
+        if (index >= 2) break;
+    }
+    std::sort(pricesArr.begin(), pricesArr.end());
+    min = pricesArr[0];
+    max = pricesArr[pricesArr.size() - 1];
+    //
 
     _getWeekRangePrice = min;
     _getWeekRangePrice.append(" - " + max);
@@ -176,52 +253,54 @@ std::string sa::get_52w_range_price(std::string stockName)
 
 std::string sa::get_absolute_change(std::string stockName)
 {
-    std::string url = "https://www.investing.com/equities/";
-    url.append(stockName);
+    std::string url = "https://www.investing.com/equities/" + stockName;
     std::shared_ptr<std::string> response = std::make_shared<std::string>();
     get_response(url, response);  // set response
 
-    std::string sign;
-    std::string val;
+    //<div class="text-xl flex items-end flex-wrap">
+    // <span class="instrument-price_change-value__h13Qh ml-2.5 text-negative-main" data-test="instrument-price-change">-0.99</span>
+    // <span class="instrument-price_change-percent__bT4yt ml-2.5 text-negative-main" data-test="instrument-price-change-percent">(-0.27%)</span>
+    //</div>
+    //
+    std::pair<std::string, std::map<std::string, std::string>> filter;
+    filter.first = "div";
+    filter.second["class"] = "text-xlflexitems-endflex-wrap"; // no spaces
+    HResponse obj1(response.get(), filter);
+    std::string s = obj1.GetFirstData(); // queue sensetive
 
-    std::vector<const char*> divTag;
-    divTag.push_back("data-test=\"instrument-price-change\"");
-    get_tagdata_from_response(divTag, response, sign);
+    filter.first = "span";
+    filter.second.clear();
+    filter.second["data-test"] = "instrument-price-change"; 
+    HResponse obj2(&s, filter);
 
-    divTag.clear();
-    divTag.push_back("data-test=\"instrument-price-change\"");
-    divTag.push_back("!--");
-    get_tagdata_from_response(divTag, response, val);
-
-    _getAbsChangePrice = sign;
-    _getAbsChangePrice.append(val);
+    _getAbsChangePrice = obj2.GetFirstData(); // queue sensetive
 
     return _getAbsChangePrice;
 }
 
 std::string sa::get_procent_change(std::string stockName)
 {
-    std::string url = "https://www.investing.com/equities/";
-    url.append(stockName);
+    std::string url = "https://www.investing.com/equities/" + stockName;
     std::shared_ptr<std::string> response = std::make_shared<std::string>();
     get_response(url, response);  // set response
 
-    std::string sign;
-    std::string val;
+    //<div class="text-xl flex items-end flex-wrap">
+    // <span class="instrument-price_change-value__h13Qh ml-2.5 text-negative-main" data-test="instrument-price-change">-0.99</span>
+    // <span class="instrument-price_change-percent__bT4yt ml-2.5 text-negative-main" data-test="instrument-price-change-percent">(-0.27%)</span>
+    //</div>
+    //
+    std::pair<std::string, std::map<std::string, std::string>> filter;
+    filter.first = "div";
+    filter.second["class"] = "text-xlflexitems-endflex-wrap"; // no spaces
+    HResponse obj1(response.get(), filter);
+    std::string s = obj1.GetFirstData(); // queue sensetive
 
-    std::vector<const char*> divTag;
-    divTag.push_back("data-test=\"instrument-price-change-percent\"");
-    divTag.push_back("!--");
-    get_tagdata_from_response(divTag, response, sign);
+    filter.first = "span";
+    filter.second.clear();
+    filter.second["data-test"] = "instrument-price-change";
+    HResponse obj2(&s, filter);
 
-    divTag.clear();
-    divTag.push_back("data-test=\"instrument-price-change-percent\"");
-    divTag.push_back("!--");
-    divTag.push_back("!--");
-    get_tagdata_from_response(divTag, response, val);
-
-    _getProcentChangePrice = sign;
-    _getProcentChangePrice.append(val);
+    _getProcentChangePrice = removeBrackets(obj2.GetLastData()); // queue sensetive
 
     return _getProcentChangePrice;
 }
@@ -235,7 +314,7 @@ static void get_response(std::string url, std::shared_ptr<std::string> resp)
         cpr::Authentication{ "user", "pass", cpr::AuthMode::BASIC },
         cpr::Parameters{ {"anon", "true"}, {"key", "value"} },
         cpr::Header{ {"accept", "application/json"}, {"User - Agent", "Mozilla / 5.0 (Windows NT 10.0; Win64; x64) AppleWebKit / 537.36 (KHTML, like Gecko) Chrome / 108.0.0.0 Safari / 537.36"} });
-    *resp = r.text;
+    *resp = r.text;  // Unicode is probably handed by CPR
 }
 
 static void get_tagdata_from_response(std::vector<const char *> tagClass, std::shared_ptr<std::string> resp, std::string &data)
@@ -265,4 +344,11 @@ static void get_tagdata_from_response(std::vector<const char *> tagClass, std::s
     if (end == std::string::npos) return;
 
     data = localResp.substr(begin + 1, end - begin - 1);
+}
+
+static std::string removeBrackets(std::string str)
+{
+    str.erase(remove(str.begin(), str.end(), '('), str.end());
+    str.erase(remove(str.begin(), str.end(), ')'), str.end());
+    return str;
 }
--- MainApp/MarketAnal.cpp
+++ MainApp/MarketAnal.cpp
@@ -15,12 +15,24 @@ MarketAnal::~MarketAnal()
 void MarketAnal::on_showMarketBtn_clicked()
 {
     StockData sd_allegro(predef::stock.allegro.stockName);
-    qDebug() << "Cena apple: " << sd_allegro.GetCurrentPrice().c_str();
-    qDebug() << "Cena apple: " << predef::stock.allegro.stockName.c_str();
+    qDebug() << "Cena " << predef::stock.allegro.stockName.c_str() << ": " << sd_allegro.GetCurrentPrice().c_str();
+    qDebug() << "prev_close_price" << sd_allegro.GetPrevClosePrice().c_str();
+    qDebug() << "open_price" << sd_allegro.GetOpenPrice().c_str();
+    qDebug() << "day_range_price" << sd_allegro.GetDayRangePrice().c_str();
+    qDebug() << "52w_range_price" << sd_allegro.Get52wRangePrice().c_str();
+    qDebug() << "absolute_change" << sd_allegro.GetAbsoluteChange().c_str();
+    qDebug() << "procent_change" << sd_allegro.GetProcentChange().c_str();
 
     StockData sd_netflix(predef::stock.netflix.stockName);
-    qDebug() << "Cena apple: " << sd_netflix.GetDayRangePrice().c_str();
-    qDebug() << "Cena apple: " << predef::stock.netflix.stockName.c_str();
+    qDebug() << "Cena " << predef::stock.netflix.stockName.c_str() << ": " << sd_netflix.GetCurrentPrice().c_str();
+    qDebug() << "prev_close_price" << sd_netflix.GetPrevClosePrice().c_str();
+    qDebug() << "open_price" << sd_netflix.GetOpenPrice().c_str();
+    qDebug() << "day_range_price" << sd_netflix.GetDayRangePrice().c_str();
+    qDebug() << "52w_range_price" << sd_netflix.Get52wRangePrice().c_str();
+    qDebug() << "absolute_change" << sd_netflix.GetAbsoluteChange().c_str();
+    qDebug() << "procent_change" << sd_netflix.GetProcentChange().c_str();
+    
+
 
     log.fnError(__FUNCTION__, "Test message");
 }
